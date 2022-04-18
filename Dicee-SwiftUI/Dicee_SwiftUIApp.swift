92481c0aa7e53b88f58a7f9928446a0f38b1d6f6
Akhil Raj <akhil.gs@gmail.com>
2022-04-18 19:27:58 -0400

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Akhil Raj on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
